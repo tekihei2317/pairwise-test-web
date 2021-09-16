a4c7ed766d9be714052bedf18999b73e1ab6b39f
tekihei2317 <tekihei2317@gmail.com>
2021-09-17 08:50:29 +0900

    feat: 計算結果を表示するための表を作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="grid-container">
+    <div class="grid">
+      <div class="grid-header">
+        <div class="grid-item"></div>
+        <div v-for="(item, index) in factors.flat()" :key="index" class="grid-item">{{ item }}</div>
+      </div>
+      <div v-for="(item, index) in factors.flat()" :key="index" class="grid-row">
+        <div class="grid-item">{{ item }}</div>
+        <div v-for="(item, index) in factors.flat()" :key="index" class="grid-item">{{ index }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive } from "vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+
+  setup() {
+    const factors = reactive([
+      ["S", "M", "L"],
+      ["Red", "Black", "White", "Blue"],
+      [1000, 3000],
+    ]);
+
+    return { factors };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.grid {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.grid-header {
+  display: flex;
+  justify-content: center;
+}
+.grid-row {
+  display: flex;
+  justify-content: center;
+}
+.grid-item {
+  height: 4rem;
+  width: 4rem;
+}
+</style>
